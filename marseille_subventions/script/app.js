da41c3050dae52f0f1bb53d002751effe5002d56
dayuum <71759046+dayuum@users.noreply.github.com>
2020-10-01 10:17:51 +0200

    Add files via upload

---
+++ marseille_subventions/script/app.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('#colonne1',{delay: 300, duration: 1300});
+ScrollReveal().reveal('#colonne2',{delay: 800, duration: 1800});
+ScrollReveal().reveal('#colonne3',{delay: 1300, duration: 1800});
+ScrollReveal().reveal('.agent', {delay:500, duration:1200, origin: 'right', distance: '60px' } );
+ScrollReveal().reveal('.circulation', {delay:500, duration:1200, origin: 'left', distance: '60px'} );
+ScrollReveal().reveal('#titre2', {delay:500, duration:1200, origin: 'top', distance: '60px'} );
+ScrollReveal().reveal('#titre3', {delay:500, duration:1200, origin: 'bottom', distance: '60px'} );
